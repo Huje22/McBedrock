0d339db872c734286474397075ae30293a3ef8c4
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-08-13 14:39:59 +0200

    Improved code

--- src/main/java/me/indian/freebedrock/RunFreeBedrock.java
+++ src/main/java/me/indian/freebedrock/RunFreeBedrock.java
@@ -1,61 +1,76 @@
 package me.indian.freebedrock;
 
-import me.indian.freebedrock.util.DownloadAssetUtil;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import me.indian.freebedrock.util.DownloadAssetUtil;
 
 public class RunFreeBedrock {
 
-    private final String sourceFile, currentDir;
+    private static final Logger LOGGER = Logger.getLogger(RunFreeBedrock.class.getName());
+    private final String takeOwnershipProFile, currentDir;
 
     public RunFreeBedrock() {
-        this.sourceFile = "C:\\Program Files (x86)\\TakeOwnershipPro\\TakeOwnershipPro.exe";
+        this.takeOwnershipProFile = "C:\\Program Files (x86)\\TakeOwnershipPro\\TakeOwnershipPro.exe";
         this.currentDir = System.getProperty("user.dir");
-        this.downloadAssets();
+
         if (!this.isWindows()) {
-            System.out.println("Twój system to nie windows!");
+            LOGGER.log(Level.SEVERE, "Twój system to nie windows!");
             System.exit(0);
+            return;
         }
         if (!this.is64Bit()) {
-            System.out.println("Twój system nie jest 64 bitowy!");
+            LOGGER.log(Level.SEVERE, "Twój system nie jest 64 bitowy!");
             System.exit(0);
+            return;
         }
 
-        final Path sourcePath = Paths.get(this.sourceFile);
+        this.downloadAssets();
+
+        final Path sourcePath = Paths.get(this.takeOwnershipProFile);
         if (!Files.exists(sourcePath)) {
-            System.out.println("Nie zainstalowałeś TakeOwnershipPro.");
-            this.installOwnership();
+            LOGGER.log(Level.SEVERE, "Nie zainstalowałeś TakeOwnershipPro.");
+
+            try {
+                this.installOwnership();
+            } catch (final IOException | InterruptedException exception) {
+                LOGGER.log(Level.SEVERE, "Nie udało się zainstalować TakeOwnershipPro");
+            }
+
             if (!Files.exists(sourcePath)) {
-                System.out.println("Nadal nie zainstalowałeś TakeOwnershipPro.");
+                LOGGER.log(Level.SEVERE, "Nadal nie zainstalowałeś TakeOwnershipPro.");
                 System.exit(0);
             }
         }
+
+        this.killAps();
         this.replaceFiles("System32");
         this.replaceFiles("SysWOW64");
-
     }
 
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         new RunFreeBedrock();
     }
 
     private void downloadAssets() {
-        final List<String> assetList = new ArrayList<>();
+        final List<String> assetList = List.of(this.currentDir + "\\System32\\Windows.ApplicationModel.Store.dll",
+                this.currentDir + "\\SysWOW64\\Windows.ApplicationModel.Store.dll");
 
-        assetList.add(this.currentDir + "\\System32\\Windows.ApplicationModel.Store.dll");
-        assetList.add(this.currentDir + "\\SysWOW64\\Windows.ApplicationModel.Store.dll");
         for (final String asset : assetList) {
             if (!Files.exists(Paths.get(asset))) {
-                System.out.println("Nie znaleziono pliku  \"Windows.ApplicationModel.Store.dll\" w aktualnym katalogu z " + asset + " !.");
-                new DownloadAssetUtil().downloadAssets();
+                LOGGER.log(Level.SEVERE, "Nie znaleziono pliku  \"Windows.ApplicationModel.Store.dll\" w aktualnym katalogu z " + asset + " !.");
+                try {
+                    DownloadAssetUtil.downloadAssets();
+                } catch (final IOException exception) {
+                    LOGGER.log(Level.SEVERE, "Nie udało się pobrać assetów!", exception);
+                }
                 return;
             }
         }
@@ -65,49 +80,64 @@ public class RunFreeBedrock {
         final String destinationFile = "C:\\Windows\\" + folderName + "\\Windows.ApplicationModel.Store.dll";
         final String replacementFile = this.currentDir + "\\" + folderName + "\\Windows.ApplicationModel.Store.dll";
 
+        final Path destinationPath = Paths.get(destinationFile);
+        final Path replacementPatch = Paths.get(replacementFile);
+
+        if (!Files.exists(replacementPatch)) {
+            LOGGER.log(Level.SEVERE, "Nie znaleziono pliku  \"Windows.ApplicationModel.Store.dll\" w aktualnym katalogu z \"" + this.currentDir + File.separator + folderName + "\" !.");
+            return;
+        }
+
         try {
-            final Path destinationPath = Paths.get(destinationFile);
-            final Path replacementPatch = Paths.get(replacementFile);
+            Runtime.getRuntime().exec(this.takeOwnershipProFile + " " + destinationFile).waitFor();
+        } catch (final InterruptedException | IOException exception) {
+            LOGGER.log(Level.SEVERE, "Nie udało się operować z TakeOwnershipPro w folderze '" + folderName + ";", exception);
+            return;
+        }
 
-            if (!Files.exists(replacementPatch)) {
-                System.out.println("Nie znaleziono pliku  \"Windows.ApplicationModel.Store.dll\" w aktualnym katalogu z \"" + this.currentDir + File.separator + folderName + "\" !.");
-                return;
+        if (Files.exists(destinationPath)) {
+            try {
+                Files.delete(destinationPath);
+            } catch (final IOException ioException) {
+                LOGGER.log(Level.SEVERE, "Nie można usunać pliku! ", ioException);
+                System.exit(0);
             }
+        }
 
-            final Process process = Runtime.getRuntime().exec(this.sourceFile + " " + destinationFile);
-            process.waitFor();
-
-            if (Files.exists(destinationPath)) {
-                try {
-                    Files.delete(destinationPath);
-                } catch (AccessDeniedException accessDeniedException) {
-                    accessDeniedException.printStackTrace();
-                    System.out.println("Nie można usunać pliku! ");
-                    System.out.println("Zamknij programy typu:!");
-                    System.out.println("Minecraft");
-                    System.out.println("Xbox app");
-                    System.out.println("Xbox game bar");
-                    System.out.println("Microsoft store");
-                    System.out.println("I inne takie a następne spróbuj ponownie");
-                    System.exit(0);
-                }
-            }
-
+        try {
             Files.copy(replacementPatch, destinationPath);
-
-            System.out.println("Plik w folderze " + folderName + " został skopiowany i podmieniony.");
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.INFO, "Plik w folderze " + folderName + " został skopiowany i podmieniony.");
+        } catch (final IOException exception) {
+            LOGGER.log(Level.SEVERE, "Nie udało się podmienić pliku w folderze '" + folderName + "'!");
+            throw new RuntimeException(exception);
         }
     }
 
-    public void installOwnership() {
-        try {
-            final Process process = Runtime.getRuntime().exec(this.currentDir + File.separator + "TakeOwnershipPro.exe");
-            process.waitFor();
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
+    public void installOwnership() throws InterruptedException, IOException {
+        Runtime.getRuntime().exec(this.currentDir + File.separator + "TakeOwnershipPro.exe").waitFor();
+    }
+
+    public void killAps() {
+        final List<String> appsToKill = List.of("XboxPcApp.exe", "WinStore.App.exe", "Minecraft.Windows.exe");
+
+        for (final String app : appsToKill) {
+            try {
+                this.killApp(app);
+            } catch (final IOException exception) {
+                LOGGER.log(Level.SEVERE, "Nie udało się zamknąć aplikacji: '" + app + "'", exception);
+            }
+        }
+    }
+
+    public void killApp(final String processName) throws IOException {
+        final ProcessBuilder processBuilder = new ProcessBuilder("taskkill", "/F", "/IM", processName);
+        final Process process = processBuilder.start();
+
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                LOGGER.log(Level.INFO, line);
+            }
         }
     }
 
--- src/main/java/me/indian/freebedrock/util/DownloadAssetUtil.java
+++ src/main/java/me/indian/freebedrock/util/DownloadAssetUtil.java
@@ -5,60 +5,54 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import javax.net.ssl.HttpsURLConnection;
 
-public class DownloadAssetUtil {
+public final class DownloadAssetUtil {
 
-    private final String saveFilePath;
+    private static final Logger LOGGER = Logger.getLogger(DownloadAssetUtil.class.getName());
 
-    public DownloadAssetUtil() {
-        this.saveFilePath = System.getProperty("user.dir") + File.separator + "assets.zip";
+    public static void downloadAssets() throws IOException {
+        final String saveFilePath = System.getProperty("user.dir") + File.separator + "assets.zip";
+
+        downloadFile(saveFilePath);
+        unzipFile(System.getProperty("user.dir"), saveFilePath);
+        Files.delete(Paths.get(saveFilePath));
+        LOGGER.log(Level.INFO, "Pobrano i rozpakowano plik ZIP.");
     }
 
-    public void downloadAssets() {
-        try {
-            downloadFile();
-            unzipFile(System.getProperty("user.dir"));
-            Files.delete(Paths.get(this.saveFilePath));
-            System.out.println("Pobrano i rozpakowano plik ZIP.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void downloadFile() throws IOException {
+    private static void downloadFile(final String saveFilePath) throws IOException {
         final URL url = new URL("https://github.com/Huje22/McBedrock/releases/download/Assets/asstes.zip");
-        final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        final HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
         final int fileSize = connection.getContentLength();
-        final InputStream inputStream = new BufferedInputStream(connection.getInputStream());
-        final FileOutputStream outputStream = new FileOutputStream(this.saveFilePath);
+        try (final InputStream inputStream = new BufferedInputStream(connection.getInputStream())) {
+            try (final FileOutputStream outputStream = new FileOutputStream(saveFilePath)) {
+                LOGGER.log(Level.INFO, "Kod połączenia: " + connection.getResponseCode());
 
-        System.out.println(connection.getResponseCode());
+                final byte[] buffer = new byte[1024];
+                int bytesRead;
+                long totalBytesRead = 0;
 
-        final byte[] buffer = new byte[1024];
-        int bytesRead;
-        long totalBytesRead = 0;
-
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
-            totalBytesRead += bytesRead;
-            final double progress = (double) totalBytesRead / fileSize * 100;
-            System.out.println("Pobrano w: " + progress + "%");
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                    totalBytesRead += bytesRead;
+                    final double progress = (double) totalBytesRead / fileSize * 100;
+                    LOGGER.log(Level.INFO, "Pobrano w: " + progress + "%");
+                }
+            }
         }
 
-        inputStream.close();
-        outputStream.close();
-
-        System.out.println("Pobrano.");
+        LOGGER.log(Level.INFO, "Pobrano.");
     }
 
-    private void unzipFile(final String targetDirectory) throws IOException {
-        try (final ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(Paths.get(this.saveFilePath)))) {
+    private static void unzipFile(final String targetDirectory, final String saveFilePath) throws IOException {
+        try (final ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(Paths.get(saveFilePath)))) {
             ZipEntry zipEntry;
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
                 final String entryName = zipEntry.getName();
@@ -66,8 +60,8 @@ public class DownloadAssetUtil {
                 if (zipEntry.isDirectory()) {
                     outputFile.mkdirs();
                 } else {
-                    try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-                        byte[] buffer = new byte[1024];
+                    try (final FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+                        final byte[] buffer = new byte[1024];
                         int length;
                         while ((length = zipInputStream.read(buffer)) > 0) {
                             outputStream.write(buffer, 0, length);
